e4edd3fa6c7a88fa67bb1629a81e3d61a515a1df
VitaliKozhurov <vitalik24021992@gmail.com>
2023-06-26 13:16:32 +0300

    add tasks reducer

--- src/api/tasksAPI.ts
+++ src/api/tasksAPI.ts
@@ -1,54 +1,68 @@
-import {todoListInstance} from './axiosInstance';
+import { todoListInstance } from "./axiosInstance";
 
-type TaskType = {
-    todoListId: string
-    id: string
-    description: string | null
-    title: string
-    status: number
-    priority: number
-    startDate: string | null
-    deadline: string | null
-    order: number
-    addedDate: string
-}
+export type TaskType = {
+    todoListId: string;
+    id: string;
+    description: string | null;
+    title: string;
+    status: number;
+    priority: number;
+    startDate: string | null;
+    deadline: string | null;
+    order: number;
+    addedDate: string;
+};
 type UpdateTaskRequestType = {
-    title: string
-    description: string
-    status: number
-    priority: number
-    startDate: string
-    deadline: string
-}
+    title: string;
+    description: string;
+    status: number;
+    priority: number;
+    startDate: string;
+    deadline: string;
+};
 type TasksDataType = {
-    item: TaskType
-}
+    item: TaskType;
+};
 type GetTasksResponseType = {
-    items: TaskType[]
-    totalCount: number
-    error: string | null
-}
+    items: TaskType[];
+    totalCount: number;
+    error: string | null;
+};
 type TasksResponseType<T = {}> = {
-    data: T
-    resultCode: number
-    messages: string[]
-    fieldsErrors?: string[]
-}
+    data: T;
+    resultCode: number;
+    messages: string[];
+    fieldsErrors?: string[];
+};
 
 export class TasksAPI {
     static getTasks(todoListID: string) {
-        return todoListInstance.get<GetTasksResponseType>(`todo-lists/${todoListID}/tasks`)
+        return todoListInstance.get<GetTasksResponseType>(
+            `todo-lists/${todoListID}/tasks`
+        );
     }
 
     static createTask(todoListID: string, title: string) {
-        return todoListInstance.post<TasksResponseType<TasksDataType>>(`todo-lists/${todoListID}/tasks`, {title})
+        return todoListInstance.post<TasksResponseType<TasksDataType>>(
+            `todo-lists/${todoListID}/tasks`,
+            { title }
+        );
     }
 
-    static updateTask(todoListID: string, taskID: string, updatedTask: UpdateTaskRequestType) {
-        return todoListInstance.put<TasksResponseType<TasksDataType>>(`todo-lists/${todoListID}/tasks/${taskID}`, updatedTask)
+    static updateTask(
+        todoListID: string,
+        taskID: string,
+        updatedTask: UpdateTaskRequestType
+    ) {
+        return todoListInstance.put<TasksResponseType<TasksDataType>>(
+            `todo-lists/${todoListID}/tasks/${taskID}`,
+            updatedTask
+        );
     }
 
     static deleteTask(todoListID: string, taskID: string) {
-        return todoListInstance.delete<TasksResponseType>(`todo-lists/${todoListID}/tasks/${taskID}`)
+        return todoListInstance.delete<TasksResponseType>(
+            `todo-lists/${todoListID}/tasks/${taskID}`
+        );
     }
 }
---
+++ src/state/tasksReducer/tasksReducer.ts
@@ -0,0 +1,123 @@
+import { v1 } from "uuid";
+import { TaskType } from "../../api/tasksAPI";
+import {
+    addTodoListAC,
+    removeTodoListAC,
+} from "../todoListReducer/todolists-reducer";
+
+export const addTasksAC = (todoListID: string, title: string) =>
+    ({
+        type: "ADD-TASK",
+        payload: { todoListID, title },
+    } as const);
+export const removeTaskAC = (todoListID: string, taskID: string) =>
+    ({
+        type: "REMOVE-TASK",
+        payload: { todoListID, taskID },
+    } as const);
+export const changeTaskTitleAC = (
+    todoListID: string,
+    taskID: string,
+    title: string
+) =>
+    ({
+        type: "CHANGE-TASK-TITLE",
+        payload: { todoListID, taskID, title },
+    } as const);
+export const changeTaskStatusAC = (
+    todoListID: string,
+    taskID: string,
+    status: number
+) =>
+    ({
+        type: "CHANGE-TASK-STATUS",
+        payload: { todoListID, taskID, status },
+    } as const);
+type TasksActionsType =
+    | ReturnType<typeof addTasksAC>
+    | ReturnType<typeof removeTaskAC>
+    | ReturnType<typeof changeTaskTitleAC>
+    | ReturnType<typeof changeTaskStatusAC>
+    | ReturnType<typeof addTodoListAC>
+    | ReturnType<typeof removeTodoListAC>;
+
+type TasksType = {
+    [key: string]: Array<TaskType>;
+};
+
+const initialState: TasksType = {};
+
+export const tasksReducer = (
+    state: TasksType = initialState,
+    action: TasksActionsType
+): TasksType => {
+    switch (action.type) {
+        case "ADD-TASK": {
+            const newTask = {
+                todoListId: action.payload.todoListID,
+                id: v1(),
+                description: "",
+                title: action.payload.title,
+                status: 0,
+                priority: 0,
+                startDate: "",
+                deadline: "",
+                order: 0,
+                addedDate: "",
+            };
+            return {
+                ...state,
+                [action.payload.todoListID]: [
+                    newTask,
+                    ...state[action.payload.todoListID],
+                ],
+            };
+        }
+        case "REMOVE-TASK": {
+            return {
+                ...state,
+                [action.payload.todoListID]: state[
+                    action.payload.todoListID
+                ].filter((task) => task.id !== action.payload.taskID),
+            };
+        }
+        case "CHANGE-TASK-TITLE": {
+            return {
+                ...state,
+                [action.payload.todoListID]: state[
+                    action.payload.todoListID
+                ].map((task) =>
+                    task.id === action.payload.taskID
+                        ? { ...task, title: action.payload.title }
+                        : task
+                ),
+            };
+        }
+        case "CHANGE-TASK-STATUS": {
+            return {
+                ...state,
+                [action.payload.todoListID]: state[
+                    action.payload.todoListID
+                ].map((task) =>
+                    task.id === action.payload.taskID
+                        ? { ...task, status: action.payload.status }
+                        : task
+                ),
+            };
+        }
+        case "ADD-TODO-LIST": {
+            return {
+                [action.payload.todoListID]: [],
+                ...state,
+            };
+        }
+        case "REMOVE-TODO-LIST": {
+            const copyState = { ...state };
+            delete copyState[action.payload.todoListID];
+            return copyState;
+        }
+
+        default:
+            return state;
+    }
+};
